a05dcefecba9b340f538d4ff8589f5e51964ef4c
leecheolu <128485402+leecheolu@users.noreply.github.com>
2025-05-21 16:00:52 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bonsai_ml',
-    version='0.0.3',
+    version='0.0.4',
     description='bonsai',
     author='MAPS',
     author_email='dongguk.maps@gmail.com',
